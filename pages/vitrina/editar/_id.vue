eaeb1bb7c18cc49b3176307544159100166ed079
Daír Sánchez <dairsanchez@gmail.com>
2020-10-18 18:26:29 -0500

    cambios

--- pages/vitrina/editar/_id.vue
+++ pages/vitrina/editar/_id.vue
@@ -2,8 +2,48 @@
   <v-app>
     <v-container>
       <v-row>
-        <v-col>
-          <v-simple-table dark>
+        <v-col v-for="producto in productos" :key="producto.id_producto">
+          <v-card elevation="24" max-width="444" class="mx-auto">
+            <!-- <v-system-bar lights-out></v-system-bar> -->
+            <v-carousel
+              :continuous="false"
+              :cycle="cycle"
+              :show-arrows="false"
+              hide-delimiter-background
+              delimiter-icon="mdi-minus"
+              height="300"
+            >
+            <!--   <v-carousel-item
+                v-for="(producto.imagenes,index) in producto"
+                :key="index"
+              >
+                <v-row class="fill-height" align="center" justify="center">
+                  <div class="display-3">{{ slide }} Slide</div>
+                </v-row>
+              </v-carousel-item> -->
+            </v-carousel>
+            <v-list two-line>
+              <v-list-item>
+                <v-list-item-avatar>
+                  <v-img
+                    src="https://cdn.vuetifyjs.com/images/john.png"
+                  ></v-img>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title>John Leider</v-list-item-title>
+                  <v-list-item-subtitle>Author</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-switch
+                    v-model="cycle"
+                    label="Cycle Slides"
+                    inset
+                  ></v-switch>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list>
+          </v-card>
+          <!--  <v-simple-table dark>
             <template v-slot:default>
               <thead>
                 <tr>
@@ -37,18 +77,7 @@
                               >
                                 <v-row>
                                   <v-col cols="12" sm="6" md="4">
-                                    <!--                                  {
- "nombreProducto": "Vestido Amarillo",
- "idComercio": 43,
- "descripcion": "Verstido verde colección Otoño",
- "tipoProducto": 2,
- "precioPVP": 18,
- "precioPorMayor": 0,
- "minimoPorMayor": 0,
- "precioVendedores": 15,
- "cantDisponible": 2,
- "tiempoEntregaHoras": 0
-} -->
+                                  
                                     <v-text-field
                                       label="Nombre"
                                       required
@@ -192,7 +221,16 @@
                 </tr>
               </tbody>
             </template>
-          </v-simple-table>
+          </v-simple-table> -->
+
+          <v-card>
+            <v-img :src="img" height="200px"></v-img>
+
+            <v-card-title> {{ nombre }} </v-card-title>
+            <v-divider></v-divider>
+
+            <v-card-text> Stock: {{ stock }} </v-card-text>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
